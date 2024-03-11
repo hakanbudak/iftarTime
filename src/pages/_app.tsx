17e8b54d8f918bc8dbd7959c1d6f17e6adb47fe9
Hakan Budak <77933239+hakanbudak@users.noreply.github.com>
2024-03-11 17:39:13 +0300

    Refactor: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-2SV4QCDXM6"></script>
+          <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2SV4QCDXM6');
+            `,
+              }}
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  );
 }
